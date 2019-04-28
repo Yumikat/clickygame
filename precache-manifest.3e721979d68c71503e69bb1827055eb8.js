4072a46094f1947a339a51daf7f6e67b7a468cf8
Pidchaya <yueyblah@hotmail.com>
2019-04-28 12:15:48 -0600

    Updates

---
+++ precache-manifest.3e721979d68c71503e69bb1827055eb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1497756e34c311e239603c49fc82cdfe",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "997880ea3c0ec2f882cc",
+    "url": "/clickygame/static/css/main.0898fcb5.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "997880ea3c0ec2f882cc",
+    "url": "/clickygame/static/js/main.9e28f33a.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
